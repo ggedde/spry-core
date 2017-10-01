d2a7cdf2ab56385fdb52c030c054155add67a92c
ggedde <geoffgedde@gmail.com>
2017-10-01 02:08:02 -0700

    Updated Version

--- src/Spry.php
+++ src/Spry.php
@@ -16,7 +16,7 @@ use stdClass;
 
 class Spry {
 
-	private static $version = "0.9.16";
+	private static $version = "0.9.17";
 	private static $routes = [];
 	private static $params = [];
 	private static $db = null;
