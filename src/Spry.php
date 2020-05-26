d364828d37a125a3f9355c5b6a1098804364f08f
ggedde <geoffgedde@gmail.com>
2020-05-26 16:29:57 -0700

    Updated Version

--- src/Spry.php
+++ src/Spry.php
@@ -37,7 +37,7 @@ class Spry
     private static $test = false;
     private static $timestart;
     private static $validator;
-    private static $version = '1.1.0';
+    private static $version = '1.1.1';
 
     /**
      * Initiates the API Call.
